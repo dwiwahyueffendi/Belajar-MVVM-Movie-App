0b149c528ff379d8fa14d168a66dd6cf9abadade
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-04-19 18:10:41 +0700

    Setup

---
+++ Latihan/V1 Setup/app/src/main/java/com/example/submissionmovieapp/data/endpoint/Movie.kt
@@ -0,0 +1,18 @@
+package com.example.submissionmovieapp.data.endpoint
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Movie(
+    val id: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: String,
+    val poster_path: String,
+    val release_date: String,
+    val vote_average: String
+): Parcelable{
+    val baseUrl: String
+    get() = "https://image.tmdb.org/t/p/w500/"
+}
