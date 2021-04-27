e1c47bffcfc6630e99b9c151819ad5fb25b9aa64
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2021-04-27 16:55:47 +0700

    Latihan

--- Submission1Jetpack/app/src/main/java/com/example/movieappjetpack/viewmodel/MovieViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.movieappjetpack.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.movieappjetpack.data.ContentEntity
-import com.example.movieappjetpack.data.dummy.DataDummy
-
-class MovieViewModel: ViewModel() {
-    fun getContentMovie(): List<ContentEntity> = DataDummy.generateDummyMovie()
-}
--- Submission1Jetpack/app/src/main/java/com/example/movieappjetpack/viewmodel/TvShowViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.movieappjetpack.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.movieappjetpack.data.ContentEntity
-import com.example.movieappjetpack.data.dummy.DataDummy
-
-class TvShowViewModel: ViewModel() {
-    fun getContentTvShow(): List<ContentEntity> = DataDummy.generateDummyTvShow()
-}
